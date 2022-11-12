65347db008c8a3b7babc8dafa6fb2268a6fd97b5
Vikram <vikram@physicsinfield.org>
2022-11-12 12:54:39 +0530

    Put on git hub

---
+++ SimTanka-iOS/Rainfall-VC/DownloadRainfallFromVC.swift
@@ -0,0 +1,14 @@
+//
+//  DownloadRainfallFromVC.swift
+//  SimTanka-iOS
+//
+//  Created by Vikram Vyas on 12/11/22.
+//
+// GNU General Public License v3.0 or later
+// https://www.gnu.org/licenses/gpl-3.0-standalone.html
+
+
+// Main class for downloading monthly rainfall
+// using Visual Crossing API
+
+import Foundation
